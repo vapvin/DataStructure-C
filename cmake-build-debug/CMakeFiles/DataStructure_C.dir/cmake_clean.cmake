a2bd78993dfb7f8d6150d04d5eda298cb953e687
vapvin <tpehdj12@daum.net>
2020-03-06 00:22:34 +0900

    Inital Commit

---
+++ cmake-build-debug/CMakeFiles/DataStructure_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure_C.dir/main.cpp.o"
+  "DataStructure_C"
+  "DataStructure_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructure_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
